8dab2817a9609897a8bc00ce78ade812f6782493
Yury Kuzma <vterkin1388@gmail.com>
2024-08-19 22:52:36 +0300

    create method isPositivNumber

--- src/main/java/ru/clevertec/StringUtils.java
+++ src/main/java/ru/clevertec/StringUtils.java
@@ -2,6 +2,14 @@ package ru.clevertec;
 
 public class StringUtils {
     public boolean isPositiveNumber(String str) {
-        return  false;
+        double number;
+
+        try {
+            number = Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            return  false;
+        }
+
+        return  number > 0;
     }
 }
