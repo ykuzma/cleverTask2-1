81ad9ccdf52afec772815b8fb91b3d90938f281f
Yury Kuzma <vterkin1388@gmail.com>
2024-08-24 14:28:05 +0300

    add jacoco, add throws NumberFormatException in the method signature

--- src/main/java/ru/clevertec/StringUtils.java
+++ src/main/java/ru/clevertec/StringUtils.java
@@ -1,16 +1,8 @@
 package ru.clevertec;
 
 public class StringUtils {
-    public boolean isPositiveNumber(String str) {
-        double number;
 
-        try {
-            number = Double.parseDouble(str);
-        } catch (NumberFormatException e) {
-            System.out.println("Be attention");
-            return false;
-        }
-
-        return  number > 0;
+    public boolean isPositiveNumber(String str) throws NumberFormatException{
+        return Double.parseDouble(str) > 0;
     }
 }
--- src/test/java/ru/clevertec/StringTest.java
+++ src/test/java/ru/clevertec/StringTest.java
@@ -1,6 +1,7 @@
+package ru.clevertec;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import ru.clevertec.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
